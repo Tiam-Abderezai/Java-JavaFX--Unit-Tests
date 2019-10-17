56a08815b6fbfc4ca4a57bfe35d3c5510d28b895
Tiam Abderezai <tiam.abderezai@gmail.com>
2019-10-17 14:25:38 -0700

    Add files via upload

---
+++ ChangeScenes/src/changescenes/Second_SceneController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package changescenes;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Tiam Abderezai
+ */
+public class Second_SceneController implements Initializable {
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    // Method "loadSecondScene" loads "Second_Scene.fxml"
+    @FXML
+    public void loadFirstScene(ActionEvent event) throws IOException {
+            Parent loadParentMain = FXMLLoader.load(getClass().getResource("First_Scene.fxml"));
+            Scene loadSceneMain = new Scene(loadParentMain);
+            Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            window.setScene(loadSceneMain);
+            window.show();
+    }
+    
+}
